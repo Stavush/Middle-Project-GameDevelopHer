4802b97cbc3c7734e8b2ffe76b58c85490fe9e57
KerenHavro <keren11222332@gmail.com>
2024-01-16 19:45:25 +0200

    added base trap sccript

---
+++ Assets/Scripts/Traps/TrapBase.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BaseTrap : MonoBehaviour, IDamagable
+{
+    public int damageAmount = 10;
+
+    public virtual void TriggerTrap(GameObject player)
+    {
+        Debug.Log("Trap triggered!");
+    }
+
+    public virtual void TakeDamage(int howMuch)
+    {
+        Debug.Log($"{gameObject.name} took {howMuch} damage.");
+    }
+
+    public  virtual void Die()
+    {
+        Debug.Log($"{gameObject.name} died");
+    }
+}
