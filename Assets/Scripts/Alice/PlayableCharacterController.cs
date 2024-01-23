7116cb3abb89d38ad3e6ec8865fc26ed524c6254
AyaHirsch <ayahirschauge@gmail.com>
2024-01-23 02:53:50 +0200

    Moving Player Child and Cinemachine
    The player (parent) is coping the capsule collider position and size of the child.
    
    The Cinamachine follows all the Player's children ( Alices)

--- Assets/Scripts/Alice/PlayableCharacterController.cs
+++ Assets/Scripts/Alice/PlayableCharacterController.cs
@@ -14,6 +14,126 @@ public class PlayableCharacterController : MonoBehaviour
     private int currentCharacterIndex;
 
 
+
+    private void Start()
+    {
+
+        switchPlayer = GetComponent<CharacterSwitch>();
+
+        foreach (var character in characters)
+        {
+            character.currentHp = character.maxHp;
+        }
+
+        characters[0].speed = 3;
+        characters[1].speed = 3;
+        characters[2].speed = 3;
+
+    }
+
+    private void Update()
+    {
+        MoveCurrentCharacter();
+
+
+        // Operate character's special ability when space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UseSpecialAbility();
+        }
+
+        // Switch a character when tab is pressed 
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwitchCharacter();
+        }
+
+    }
+
+    public void SwitchCharacter()
+    {
+        currentCharacterIndex = (currentCharacterIndex + 1) % characters.Length;
+        SetActiveCharacter(currentCharacterIndex);
+        SwitchActiveChild(currentCharacterIndex);
+    }
+
+    public void SetActiveCharacter(int index)
+    {
+        switchPlayer.SwitchCharacter(currentCharacterIndex);
+    }
+
+    public void ApplyCharacterDamage(IDamagable damagable)
+    {
+        characters[currentCharacterIndex].ApplyDamage(damagable);
+    }
+
+    public void MoveCurrentCharacter() //Sarin changed it
+    {
+        //CencelBoxColiderForMovementToNotBeDisturbed(); // sarin added a function
+        UpdateParentColliderFromActiveChild();
+        //gameObject.GetComponent<PlayableCharacterModel>().speed = characters[currentCharacterIndex].speed;
+        //gameObject.GetComponent<PlayableCharacterModel>().Movement();
+    }
+
+    public void UseSpecialAbility()
+    {
+        characters[currentCharacterIndex].SpecialAbility();
+    }
+
+    //sarin addeed
+    /*private void CencelBoxColiderForMovementToNotBeDisturbed() // and make sure they are moving together and not move around
+    {
+        for (int i = 0; i < characters.Length; i++)
+        {
+            if (currentCharacterIndex != i)
+            {
+                characters[i].gameObject.GetComponent<BoxCollider2D>().enabled = false;
+                characters[i].gameObject.transform.position = characters[currentCharacterIndex].gameObject.transform.position;
+            }
+            else if (currentCharacterIndex == i)
+            {
+                characters[i].gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            }
+            characters[i].transform.position = transform.position;
+        }
+
+    }*/
+
+    private void UpdateParentColliderFromActiveChild()
+    {
+        if (currentCharacterIndex >= 0 && currentCharacterIndex < characters.Length)
+        {
+            CapsuleCollider2D activeChildCollider = characters[currentCharacterIndex].gameObject.GetComponent<CapsuleCollider2D>();
+            CapsuleCollider2D parentCollider = GetComponent<CapsuleCollider2D>();
+
+            if (activeChildCollider != null && parentCollider != null)
+            {
+                // Copy the size and offset of the active child's capsule collider to the parent
+                parentCollider.size = activeChildCollider.size;
+                parentCollider.offset = activeChildCollider.offset;
+            }
+        }
+    }
+
+    // Call this function whenever you switch the active child
+    private void SwitchActiveChild(int newIndex)
+    {
+        currentCharacterIndex = newIndex;
+        UpdateParentColliderFromActiveChild();
+    }
+
+
+
+}
+
+/*public class PlayableCharacterController : MonoBehaviour
+{
+    private CharacterSwitch switchPlayer;
+
+    [SerializeField] private PlayableCharacterModel[] characters = new PlayableCharacterModel[3];
+    private int currentCharacterIndex;
+
+
     private void Start()
     {
         switchPlayer = GetComponent<CharacterSwitch>();
@@ -72,5 +192,5 @@ public class PlayableCharacterController : MonoBehaviour
     {
         characters[currentCharacterIndex].SpecialAbility();
     }
-}
+}*/
 
--- Assets/Scripts/Alice/PlayableCharacterModel.cs
+++ Assets/Scripts/Alice/PlayableCharacterModel.cs
@@ -3,7 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class PlayableCharacterModel : MonoBehaviour, IDamagable
+public class PlayableCharacterModel : MonoBehaviour, IDamagable
+{
+
+    public int speed;
+    public int maxHp = 3;
+    public int currentHp;
+
+    /*public virtual void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput) * speed * Time.deltaTime;
+        transform.Translate(movement, Space.Self);
+    }*/
+
+    // change abstract to virtual if we want all characters to act the same
+    public virtual void ApplyDamage(IDamagable damagable)
+    {
+
+    }
+
+    public virtual void SpecialAbility()
+    {
+
+    }
+
+    // Inheritance from IDamagable and implementation of it
+    public void TakeDamage(int howMuch)
+    {
+        currentHp -= howMuch;
+        if (currentHp <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("Alice died :( ");
+    }
+
+}
+/*public abstract class PlayableCharacterModel : MonoBehaviour, IDamagable
 {
 
     public int speed;
@@ -38,4 +81,4 @@ public abstract class PlayableCharacterModel : MonoBehaviour, IDamagable
     {
         Debug.Log("Alice died :( ");
     }
-}
+}*/
